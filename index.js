c5b999e6ae6f7c6748b6253e567514687536c5c4
Lizzie Anthony <lizziewrab@me.com>
2021-11-01 19:30:49 -0500

    commit tree dac48605dbc35453f534c9e58137310b9c64972e

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+    let num1 = 2
+    let num2 = 31
+    const multiply = num1 * num2
+
+    let random = Math.round(Math.random() + 10)
+    console.log(random)
+
+    let num3 = 8
+    let num4 = 4
+    const mod = num3-num4
+
+    const max = Math.max(5,10,15,20)
